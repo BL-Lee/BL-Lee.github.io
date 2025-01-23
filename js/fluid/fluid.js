dddb26daa66b688727b16b524e1ee845ea03cb90
BL-Lee <bn628547@dal.ca>
2025-01-23 12:45:10 -0400

    fluids

--- js/fluid/fluid.js
+++ js/fluid/fluid.js
@@ -65,6 +65,7 @@ fn to_out_color(angle: f32, magnitude: f32) -> vec4f {
   return output;
 }
 
+
        @group(0) @binding(0) var readVel: texture_storage_2d<rgba32float, read>;
        @group(0) @binding(1) var writeVel: texture_storage_2d<rgba32float, write>;
        @group(0) @binding(2) var<uniform> uTime: timeUniform;
@@ -74,37 +75,100 @@ fn to_out_color(angle: f32, magnitude: f32) -> vec4f {
        ) {
 
           let time = uTime.time;
-          let vid = id.x * 2 + u32(uTime.buffer);
-          let coord = vec2u(vid % 127, vid / 127);
+          //let vid = id.x * 2 + u32(uTime.buffer);
+          //let coord = vec2u(vid % 127, vid / 127);
+          let coord = vec2u(id.xy);
           let topOut = textureLoad(readVel, vec2u(coord + vec2u(0,1)));
           let leftOut = textureLoad(readVel, vec2u(vec2i(coord) + vec2i(-1,0)));
           let bottomOut = textureLoad(readVel, vec2u(vec2i(coord) + vec2i(0,-1)));
           let rightOut = textureLoad(readVel, vec2u(coord + vec2u(1,0)));
           let prevOut = textureLoad(readVel, coord);
-          let input = vec4f(leftOut.r, bottomOut.g, rightOut.b, topOut.a);
-          //let input = vec4f(rightOut.r - prevOut.r, -prevOut.g + topOut.g, 0.0, 1.0);
-          let totalInput = leftOut.r + bottomOut.g + rightOut.b + topOut.a;
-          //let totalInput = topOut.g - prevOut.r - prevOut.g + rightOut.r;
 
-          let output = vec4f(totalInput / 4.0);
+//Forces and diffusion
+
+          var totalInput = leftOut.r + bottomOut.g - rightOut.r - topOut.g;
+
+          totalInput *= 1.0;
+          var s = vec4f(1.0, 1.0, 1.0, 1.0);
+          if (coord.x >= 126)
+          {
+            s.z = 0.0;
+          }
+          if (coord.x <= 0)
+          {
+            s.x = 0.0;
+          }
+          if (coord.y >= 126)
+          {
+            s.y = 0.0;
+          }
+          if (coord.y <= 0)
+          {
+            s.w = 0.0;
+          }
+
+          let gridWidth = 1.0;
+
+          let pressure = vec4f(rightOut.b * s.z, leftOut.b * s.x,
+                                       topOut.b * s.y, bottomOut.b * s.a);
+          
+          let pressureGradient = vec2f(pressure.x - pressure.y,
+                                       pressure.z - pressure.w);
+
+          let pressureVal = pressure.x + pressure.y + pressure.z + pressure.w;
+
+          let sVal = s.x + s.y + s.z + s.w;
+          var output = vec4f(0.0,0.0,
+                              (totalInput + pressureVal) / sVal,
+                              1.0);
+ 
+          //advect velocity
+
+          var tracedPos = (vec2f(coord)) - uTime.dt * prevOut.xy;
+          var tracedTexel = vec2u(tracedPos);
+          //Might need to center to pixel
+
+          var mixVal = fract(tracedPos);
+
+          let bl = textureLoad(readVel, tracedTexel + vec2u(0,0)).rg;
+          let br = textureLoad(readVel, tracedTexel + vec2u(1,0)).rg;
+          let tl = textureLoad(readVel, tracedTexel + vec2u(0,1)).rg;
+          let tr = textureLoad(readVel, tracedTexel + vec2u(1,1)).rg;
+
+          let advectedVel = mix(mix(bl,br, mixVal.x), mix(tl, tr, mixVal.x), mixVal.y);
+
+          output.x = advectedVel.x;
+          output.y = advectedVel.y;
+
+
 
           var color = output;
+          var inPix = 0.0;
           if (coord.x == 65 && coord.y == 55)
           {
-             color = vec4(3.0,0.0,0.0,0.0);
+             color = vec4(200.0,2.0,0.0,1.0);
+             inPix = 1.0;
           }
           if (coord.x >= 96 && coord.x < 105 && coord.y > 90 && coord.y < 96)
           {
-             color = vec4(0.0,1.0,0.0,0.0);
-
+             color = vec4(12.0,1.0,0.0,1.0);
+             inPix = 1.0;
           }
+
           
           textureStore(writeVel, coord, color);
 
-           let angle = atan2(leftOut.r - rightOut.b, bottomOut.g - topOut.a);
+          //let angle = atan2(pressureGradient.x, pressureGradient.y);
+          let angle = atan2(output.x, output.y);
            
-           let outColor = to_out_color(180 *  angle / 3.1415 , min(1.0,totalInput * 8.0));
-//          let outColor = (input + 1.0) / 2.0;
+          //let outColor = to_out_color(180 *  angle / 3.1415 , min(1.0, abs(output.b)));
+
+          //let outColor = vec4f((1.0 + output.b) / 2.0, inPix, 0.0, 1.0);
+          let outColor = vec4f((pressureGradient + 1.0) / 2.0, 1.0, 1.0);
+          //let outColor = vec4f((pressureVal + 1.0) / 2.0);
+          //let outColor = vec4f((color.xy + 1.0) / 2.0, inPix, 1.0);
+          //let outColor = vec4f(vec2f(tr.xy) / 127.0, bl.xy);
+          //let outColor = vec4f((mixVal.xy + 1.0) / 2.0, inPix, 1.0);
           textureStore(swapChain, coord, outColor);
       }
     `,
@@ -169,7 +233,7 @@ function endRenderPass(device, encoder)
 
 function render(info)
 {
-    info.timeValues.set([info.timeValues[0] + 0.032, 0.032, 1.0], 0);
+    info.timeValues.set([info.timeValues[0] + 0.032, 0.32, 1.0], 0);
     info.device.queue.writeBuffer(info.uniformBuffer, 0, info.timeValues);
     const canvasTexture = info.context.getCurrentTexture();
 
@@ -182,12 +246,13 @@ function render(info)
 	renderPass.setPipeline(info.pipeline);
 	renderPass.setBindGroup(0, bindGroup);
 
-	renderPass.dispatchWorkgroups(canvasTexture.width * canvasTexture.height / 2);
+	//renderPass.dispatchWorkgroups(canvasTexture.width * canvasTexture.height / 2);
+	renderPass.dispatchWorkgroups(canvasTexture.width, canvasTexture.height);
 	renderPass.end(info.device, encoder);
 	endRenderPass(info.device, encoder);
     }
 
-    info.timeValues.set([info.timeValues[0], 0.032, 0.0], 0);
+/*    info.timeValues.set([info.timeValues[0], 0.032, 0.0], 0);
     info.device.queue.writeBuffer(info.uniformBuffer, 0, info.timeValues);
      //bindGroup = initUniforms(info.device, info.pipeline,
 //				   info.readBuffer, info.writeBuffer,
@@ -203,7 +268,7 @@ function render(info)
 	endRenderPass(info.device, encoder);
 
     }
-
+*/
 /*
     const copyEncoder = info.device.createCommandEncoder({label: "copy to swapchain"})
     copyEncoder.copyTextureToTexture({texture : info.writeBuffer},
@@ -254,7 +319,7 @@ async function setup()
 		      readBuffer,
 		      writeBuffer, timeValues,
 		      uniformBuffer};
-
+/*
     const interval = setInterval(function() {
 	//Swap read/write buffers
 	let swap = globalInfo.readBuffer;
@@ -263,9 +328,24 @@ async function setup()
 
 	render(globalInfo);
     }, 32);
-
+*/
     //render(globalInfo);
     
     return globalInfo;
 }
-setup();
+
+setup().then((globalInfo) =>
+    {
+addEventListener("keypress", function(event) {
+        //Swap read/write buffers
+	let swap = globalInfo.readBuffer;
+	globalInfo.readBuffer = globalInfo.writeBuffer;
+	globalInfo.writeBuffer = swap;
+
+	render(globalInfo);
+    
+});
+	
+    });
+
+
